da5f6376d70cde233b79fa875d85f285889ef80f
Carlos Nobrega <nobregacarlosjr@gmail.com>
2020-11-24 00:30:57 -0300

    feat: save file with item

--- src/infra/GerentePersistenciaFile.java
+++ src/infra/GerentePersistenciaFile.java
@@ -1,14 +1,14 @@
 package infra;
 
+import java.io.*;
 import java.util.TreeSet;
 import business.model.Usuario;
 import business.model.Data;
+import business.model.Pedido;
+import java.util.ArrayList;
 import java.util.*;
-import java.io.FileWriter;
-import java.io.FileReader;
-import java.io.BufferedWriter;
-import java.io.BufferedReader;
-import java.io.IOException;
+
+import jdk.nashorn.internal.parser.JSONParser;
 import util.PersistenciaException;
 
 public class GerentePersistenciaFile implements GerentePersistencia{
@@ -23,11 +23,27 @@ public class GerentePersistenciaFile implements GerentePersistencia{
                 String login = usuario[0];
                 String senha = usuario[1];
                 String data = usuario[2].split("\n")[0];
+                ArrayList<Pedido> pedidosArrayList = new ArrayList<Pedido>();
+                if (usuario.length > 3) {
+                    String[] pedidos = usuario[3].split("# ");
+                    for (int i = 0; i < pedidos.length; i++ ) {
+                        if (pedidos[i].equals("")) {
+                            continue;
+                        }
+                        String[] dados = pedidos[i].split(" ");
+                        String item = dados[0];
+                        double valor = Double.valueOf(dados[1]);
+                        String[] itens = {item};
+                        pedidos[i].split(" ");
+                        pedidosArrayList.add(new Pedido(itens, valor));
+                    }
+                }
                 int dia = Integer.parseInt(data.split("/")[0]);
                 int mes = Integer.parseInt(data.split("/")[1]);
                 int ano = Integer.parseInt(data.split("/")[2]);
                 //Data d = new Data(dia,mes,ano);
-                Usuario u = new Usuario(login, senha, new Data(dia,mes,ano));
+
+                Usuario u = new Usuario(login, senha, new Data(dia,mes,ano), pedidosArrayList);
                 usuarios.add(u);
             }
         } catch (IOException exception) {
@@ -46,7 +62,13 @@ public class GerentePersistenciaFile implements GerentePersistencia{
                 String login = usuario.getLogin();
                 String senha = usuario.getSenha();
                 String data = usuario.getData().toString();
-                out.write(login + "\t" + senha + "\t" + data + "\n");
+                ArrayList<Pedido> pedidos = usuario.getPedidos();
+                String pedidosOutput = "";
+                for (int i = 0; i < pedidos.size(); i++) {
+                    pedidosOutput +=  "# " + String.join(" ", pedidos.get(i).getItens()) + " ";
+                    pedidosOutput +=  pedidos.get(i).getValor();
+                }
+                out.write(login + "\t" + senha + "\t" + data + "\t" + pedidosOutput + "\n");
                 out.flush();
             }
     
